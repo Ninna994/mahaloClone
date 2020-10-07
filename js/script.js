42866b55957789b4c7c1acb2f7b2afa2923169e1
Ninna994 <inanikolina@gmail.com>
2020-10-07 20:22:59 +0200

    Website text added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $('body').scrollspy({
     target: "#header"
 })
+
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
